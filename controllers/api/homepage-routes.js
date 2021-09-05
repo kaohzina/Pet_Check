02ae4b9f1962b66233c86e7451cde4fcd7350ec4
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-05 10:48:10 -0500

    semi working again

---
+++ controllers/api/homepage-routes.js
@@ -0,0 +1,8 @@
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const router = require('express').Router();
+// Homepage Route:
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,70 +1,47 @@
-// const express = require('express');
-// const mysql2 = require('mysql2');
-// const path = require('path');
-// const exphbs = require('express-handlebars');
-// const logger = require('./middleware/logger');
-// // get access to the array:
-// const members = require('./Members');
-
-// const app = express();
-
-
-// // Init middleware
-// // app.use(logger);
-
-// // Handlebars Middleware:
-// //set theview engine to handlebars pass in the exphbs
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// //setting the view engine
-// app.set('view engine', 'handlebars');
-
-// // Body Parser Middleware
-// app.use(express.json());
-// // handle url encoding data
-// app.use(express.urlencoded({ extended: false }));
-
-// // connect to the database
-
-// // Homepage Route:
-// app.get('/', (req, res) => res.render('index', {
-//     title: 'Animal Companion App',
-//     members
-// }));
-
-// // route to the users pet profile
-
-// // Set static folder
-// // hint: move after the Homepage route to see another render of the server
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // members api routes
-// app.use('/api/members', require('./routes/api/members-route'));
-
-// const PORT = process.env.PORT || 3001;
-
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-// //turn on connection to db and server
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(PORT, () => console.log('Now listening'));
-//   });
-
-
-
-  const express = require('express');
-const routes = require('./routes');
+const express = require('express');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+// const logger = require('./middleware/logger');
+// const members = require('./Members');
 
 const app = express();
 const PORT = process.env.PORT || 3001
 
+// Init middleware
+// app.use(logger);
+
+// Handlebars Middleware:
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
 
+app.get('/', (req, res) => res.render('index', {
+    title: 'Animal Companion App',
+    members
+  }));
+// members api routes
+app.use('/api/user', require('./controllers/api/user-routes'));
+
+
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+
+
+
+
+
+
+
