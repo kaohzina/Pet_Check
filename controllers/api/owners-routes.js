3feea713ae33c57f7e374463f6c9d86489b3aee4
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-08 12:40:03 -0500

    working password detection, that ensures the password is correct for the owners account

--- controllers/api/owners-routes.js
+++ controllers/api/owners-routes.js
@@ -51,20 +51,23 @@ router.post('/', (req, res) => {
 
 router.post('/login', (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
-    User.findOne({
+    Owner.findOne({
       where: {
         email: req.body.email
       }
-    }).then(dbUserData => {
-      if (!dbUserData) {
-        res.status(400).json({ message: 'No user with that email address!' });
+    }).then(dbOwnerData => {
+      if (!dbOwnerData) {
+        res.status(400).json({ message: 'No owners with that email address!' });
         return;
       }
-  
-      res.json({ user: dbUserData });
-  
       // Verify user
-  
+      const validPassword = dbOwnerData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+      
+      res.json({ user: dbOwnerData, message: 'You are now logged in!' });
     });  
   });
 
