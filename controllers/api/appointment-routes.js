069ca3e494ee222a30770ea253a4338cf0017e54
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-08 13:36:21 -0500

    appointment times for the pets and owner

---
+++ controllers/api/appointment-routes.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Appointment extends Model {}
+
+Appointment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    owner_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Owner',
+        key: 'id'
+      }
+    },
+    pet_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'pet',
+        key: 'id'
+      }
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    time: {
+      type: DataTypes.TIME,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'appointment'
+  }
+);
+
+module.exports = Appointment;
