21fd6ee2a8fecbef7b207f908011a06b87101beb
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-10 12:57:10 -0500

    Somethings not working here for some reason

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,34 @@
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Owner, Pet, Appointment, Description } = require('../models');
 const router = require('express').Router();
+
 // Homepage Route:
 router.get('/', (req, res) => {
-  res.render('homepage');
+  Appointment.findAll({
+    attributes: [
+      'id',
+      'owner_id',
+      'pet_id',
+      'data',
+      'time'
+    ],
+    include: [
+      {
+        model: Owner,
+        attributes: ['fname', 'lname', 'email'],
+        include: {
+          model: Pet,
+          attributes: ['name', 'type', 'breed']
+        }
+      }
+    ]
+  })
+    .then(dbAppointmentData => {
+      res.render('homepage', dbAppointmentData[0]);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
 });
 
 router.get('/register', (req, res) => {
