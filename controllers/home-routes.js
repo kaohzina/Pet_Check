8af1381781f47cd0edd400f7c598fec8bf63fae1
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-11 12:03:37 -0500

    fix the const at the top of home-routes page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Pet, Owner, Description } = require('../models');
 const router = require('express').Router();
 // Homepage Route:
 router.get('/', (req, res) => {
