9a5870d96ea715bfcc0b656c8ddbcb973a11bb76
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-11 12:14:38 -0500

    vote to pet trying to fix the pet check db

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
       'type',
       'breed',
       'age',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE Pet.id = Appointment.pet_id)'), 'Pet_Appointment']
+      [sequelize.literal('(SELECT COUNT(*) FROM Pet WHERE Pet.id = Appointment.pet_id)'), 'Pet_Appointment']
     ],
     include: [
       {
