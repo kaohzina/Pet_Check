9dfbcb459fddbd932a7b65b5040ba3367db359c3
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-11 13:32:58 -0500

    fixing the home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,27 +1,10 @@
 const sequelize = require('../config/connection');
-<<<<<<< HEAD
-const { Owner, Pet, Appointment, Description } = require('../models');
-=======
-<<<<<<< HEAD
-const { Pet, Owner, Description } = require('../models');
-=======
-const { Pet, Owner } = require('../models');
->>>>>>> controller
->>>>>>> develop
+const { Owner, Pet, Appointment} = require('../models');
 const router = require('express').Router();
 
 // Homepage Route:
 
 router.get('/', (req, res) => {
-<<<<<<< HEAD
-  Appointment.findAll({
-    attributes: [
-      'id',
-      'owner_id',
-      'pet_id',
-      'data',
-      'time'
-=======
   console.log('======================');
   Pet.findAll({
     attributes: [
@@ -31,27 +14,10 @@ router.get('/', (req, res) => {
       'breed',
       'age',
       [sequelize.literal('(SELECT COUNT(*) FROM appointment WHERE Pet.id = Appointment.pet_id)'), 'Pet_Appointment']
->>>>>>> develop
     ],
     include: [
       {
         model: Owner,
-<<<<<<< HEAD
-        attributes: ['fname', 'lname', 'email'],
-        include: {
-          model: Pet,
-          attributes: ['name', 'type', 'breed']
-        }
-      }
-    ]
-  })
-    .then(dbAppointmentData => {
-      res.render('homepage', dbAppointmentData[0]);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    })
-=======
         attributes: ['fname', 'lname']
       }
     ]
@@ -68,7 +34,6 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
->>>>>>> develop
 });
 
 // get single Pet
