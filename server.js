4afcba175a66b14f51a5da6bb58c18facc9147fc
Rugger11 <82486396+zimmermanc6588@users.noreply.github.com>
2021-09-12 22:33:41 -0500

    quitting too much old code

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method"));
 
 // turn on routes
 app.use(require('./controllers/'));
